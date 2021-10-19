a6359f73eb30a7b061a85750d2177e961ed68248
Andy <bobopop787@gmail.com>
2021-10-19 17:57:06 -0400

    Created Motorcycle.java

---
+++ src/com/bobopop787/Motorcycle.java
@@ -0,0 +1,19 @@
+package com.bobopop787;
+
+import java.awt.Color;
+
+public class Motorcycle {
+	private String modelId;
+	private Color color;
+	private int year;
+	private double mileage;
+	private double speed;
+	private double price;
+	private double maxSpeed;
+	
+	public Motorcycle() {
+		modelId = "Default";
+		color = Color.BLACK;
+		
+	}
+}
