18a7997e2cd955f4098901e6d89cae7324892577
Andy <bobopop787@gmail.com>
2021-10-19 21:40:59 -0400

    Modified Motorcycle.java
    Added fields and constructors

--- src/com/bobopop787/Motorcycle.java
+++ src/com/bobopop787/Motorcycle.java
@@ -1,6 +1,7 @@
 package com.bobopop787;
 
 import java.awt.Color;
+import java.util.Calendar;
 
 public class Motorcycle {
 	private String modelId;
@@ -14,6 +15,20 @@ public class Motorcycle {
 	public Motorcycle() {
 		modelId = "Default";
 		color = Color.BLACK;
-		
+		year = Calendar.getInstance().get(Calendar.YEAR);
+		mileage = 0;
+		speed = 0;
+		price = 50000;
+		maxSpeed = 100;
+	}
+	
+	public Motorcycle(String modelId, Color color, int year, double mileage, double maxSpeed, double price) {
+		this.modelId = modelId;
+		this.color = color;
+		this.year = year;
+		this.mileage = mileage;
+		this.speed = 0;
+		this.price = price;
+		this.maxSpeed = maxSpeed;
 	}
 }
