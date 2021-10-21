1f8f4b7eddbe866e4edb113c656f3d400165df17
bobopop787 <bobopop787@gmail.com>
2021-10-21 16:19:55 -0400

    Modified Motorcycle.java
    Added public void move(), public void setSpeed(), public void printInfo()

--- src/com/bobopop787/Motorcycle.java
+++ src/com/bobopop787/Motorcycle.java
@@ -31,4 +31,28 @@ public class Motorcycle {
 		this.price = price;
 		this.maxSpeed = maxSpeed;
 	}
+	
+	public void move() {
+		mileage += speed;
+	}
+	
+	public void setSpeed(double speed) {
+		if(speed > maxSpeed) {
+			this.speed = maxSpeed;
+		} else if(speed < 0) {
+			this.speed = 0;
+		} else {
+			this.speed = speed;
+		}
+	}
+	
+	public void printInfo() {
+		System.out.println("Model: " + modelId);
+		System.out.println("Color(rgb): " + color.getRed() + ", " + color.getGreen() + ", " + color.getBlue());
+		System.out.println("Year: " + year);
+		System.out.println("Mileage: " + mileage);
+		System.out.println("Speed: " + speed);
+		System.out.println("Price: " + price);
+		System.out.println("Maximum Speed: " + maxSpeed);
+	}
 }
